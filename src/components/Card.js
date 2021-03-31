8db0664bd558521954349d3359a5629c87add107
Richard Buschbeck <richard.bu@web.de>
2021-03-31 16:00:53 +0200

    Random generation of robots based on current date

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Card = (props) => {
+
     return(
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robot' src={`https://robohash.org/${props.id}?200x200`}/>
+            <img alt='robot' src={`https://robohash.org/${Date()}${props.id}?200x200`}/>
             <div>
                 <h2>
                     {props.name}
