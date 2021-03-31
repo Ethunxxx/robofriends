c55dea15bd2c20e645be0de0d4ff1e67fb31bcba
Richard Buschbeck <richard.bu@web.de>
2021-03-31 13:36:37 +0200

    Complete robofriends project

---
+++ src/containers/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+// import { robots } from './robots';
+import Scroll from '../components/Scroll'
+import './App.css';
+
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: '',
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value})
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then( response => response.json())
+            .then( users => this.setState({ robots: users }));
+    }
+
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            
+        })
+
+        if(!this.state.robots.length){
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <h1 className='f3'>Loading...</h1>
+                </div>
+            )
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
+
+        
+    }
+    
+}
+
+
+export default App;
